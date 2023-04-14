35c53c547a805f570afc65d89e60d0d5e5c108b9
DaneFiqri <marzetillah12@gmail.com>
2023-04-15 00:58:46 +0700

    Tugas_WP2

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,19 @@
+<?php
+class Matakuliah extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+    
+    public function cetak()
+    {
+        this->form_validation->set_rules9'kode', 'Kode Matakuliah', 'required|min_length[3]', ['']
+        $data = [
+            'kode' => $this->input->post('kode'),
+            'nama' => $this->input->post('nama'),
+            'sks' => $this->input->post('sks')
+        ];
+        $this->load->view('view-data-matakuliah', $data);
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : D</li>
+        <li>Nama Belakang : Aneru</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> JL. </li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Jakarta, 19 Desember 1991</li>
+</ul>
+ <h4>Hobi</h4>
+ <ul type="square">
+    <li>Bangun Siang</li>
+    <li>Weaboo</li>
+</ul>
+</section>
