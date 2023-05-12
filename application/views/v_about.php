f96d869bb5ae51952d5d16186d03675fc8cb7d63
DaneFiqri <marzetillah12@gmail.com>
2023-05-12 15:24:08 +0700

    Tugas_WP3

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,22 +2,23 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : D</li>
-        <li>Nama Belakang : Aneru</li>
+        <li>Nama Depan : Zidane</li>
+        <li>Nama Belakang : Fiqriansyah</li>
 </ul>
 <br>
 <h4>Alamat</h4>
 <ul type="none">
-    <li> JL. </li>
+    <li> JL. Sabaody No. 03</li>
 </ul>
 
-<h4>Tempat Lahir</h4>
-<ul type="none">
+<h4>Tempat, tanggal Lahir</h4>
+<ul type="disc">
     <li>Jakarta, 19 Desember 1991</li>
 </ul>
  <h4>Hobi</h4>
  <ul type="square">
     <li>Bangun Siang</li>
+    <li>Running</li>
     <li>Weaboo</li>
 </ul>
 </section>
